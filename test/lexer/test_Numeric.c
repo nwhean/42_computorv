2e18ebde0f300bddae93ee5b85604f1a282e5e04
Ng Wee Hean <heanconcerto@gmail.com>
2024-12-25 21:49:24 +0800

    Ensured Numeric works with GMP library

--- test/lexer/test_Numeric.c
+++ test/lexer/test_Numeric.c
@@ -16,8 +16,8 @@ void test_iadd(void) {
 	struct s_Rational	*a = Rational_from_long(1, 2);
 	struct s_Rational	*b = Rational_from_long(1, 1);
 	numeric_iadd((void **)&a, a);
-	TEST_ASSERT_EQUAL(a->numerator, b->numerator);
-	TEST_ASSERT_EQUAL(a->denominator, b->denominator);
+	TEST_ASSERT_EQUAL(mpz_get_si(a->numerator), mpz_get_si(b->numerator));
+	TEST_ASSERT_EQUAL(mpz_get_si(a->denominator), mpz_get_si(b->denominator));
 	delete(a);
 	delete(b);
 }
@@ -26,8 +26,8 @@ void test_isub(void) {
 	struct s_Rational	*a = Rational_from_long(1, 2);
 	struct s_Rational	*b = Rational_from_long(0, 1);
 	numeric_isub((void **)&a, a);
-	TEST_ASSERT_EQUAL(a->numerator, b->numerator);
-	TEST_ASSERT_EQUAL(a->denominator, b->denominator);
+	TEST_ASSERT_EQUAL(mpz_get_si(a->numerator), mpz_get_si(b->numerator));
+	TEST_ASSERT_EQUAL(mpz_get_si(a->denominator), mpz_get_si(b->denominator));
 	delete(a);
 	delete(b);
 }
@@ -36,8 +36,8 @@ void test_imul(void) {
 	struct s_Rational	*a = Rational_from_long(1, 2);
 	struct s_Rational	*b = Rational_from_long(1, 4);
 	numeric_imul((void **)&a, a);
-	TEST_ASSERT_EQUAL(a->numerator, b->numerator);
-	TEST_ASSERT_EQUAL(a->denominator, b->denominator);
+	TEST_ASSERT_EQUAL(mpz_get_si(a->numerator), mpz_get_si(b->numerator));
+	TEST_ASSERT_EQUAL(mpz_get_si(a->denominator), mpz_get_si(b->denominator));
 	delete(a);
 	delete(b);
 }
@@ -46,8 +46,8 @@ void test_idiv(void) {
 	struct s_Rational	*a = Rational_from_long(1, 2);
 	struct s_Rational	*b = Rational_from_long(1, 1);
 	numeric_idiv((void **)&a, a);
-	TEST_ASSERT_EQUAL(a->numerator, b->numerator);
-	TEST_ASSERT_EQUAL(a->denominator, b->denominator);
+	TEST_ASSERT_EQUAL(mpz_get_si(a->numerator), mpz_get_si(b->numerator));
+	TEST_ASSERT_EQUAL(mpz_get_si(a->denominator), mpz_get_si(b->denominator));
 	delete(a);
 	delete(b);
 }
@@ -56,18 +56,18 @@ void test_imod(void) {
 	struct s_Rational	*a = Rational_from_long(22, 7);
 	struct s_Rational	*b = Rational_from_long(0, 1);
 	numeric_imod((void **)&a, a);
-	TEST_ASSERT_EQUAL(a->numerator, b->numerator);
-	TEST_ASSERT_EQUAL(a->denominator, b->denominator);
+	TEST_ASSERT_EQUAL(mpz_get_si(a->numerator), mpz_get_si(b->numerator));
+	TEST_ASSERT_EQUAL(mpz_get_si(a->denominator), mpz_get_si(b->denominator));
 	delete(a);
 	delete(b);
 }
 
 void test_ineg(void) {
 	struct s_Rational	*a = Rational_from_long(22, 7);
-	struct s_Rational	*b = Rational_from_long((long)-22, 7);
+	struct s_Rational	*b = Rational_from_long((long)-22, 7);;
 	numeric_ineg((void **)&a);
-	TEST_ASSERT_EQUAL(a->numerator, b->numerator);
-	TEST_ASSERT_EQUAL(a->denominator, b->denominator);
+	TEST_ASSERT_EQUAL(mpz_get_si(a->numerator), mpz_get_si(b->numerator));
+	TEST_ASSERT_EQUAL(mpz_get_si(a->denominator), mpz_get_si(b->denominator));
 	delete(a);
 	delete(b);
 }
@@ -76,8 +76,8 @@ void test_ipow(void) {
 	struct s_Rational	*a = Rational_from_long(3, 1);
 	struct s_Rational	*b = Rational_from_long(27, 1);
 	numeric_ipow((void **)&a, a);
-	TEST_ASSERT_EQUAL(a->numerator, b->numerator);
-	TEST_ASSERT_EQUAL(a->denominator, b->denominator);
+	TEST_ASSERT_EQUAL(mpz_get_si(a->numerator), mpz_get_si(b->numerator));
+	TEST_ASSERT_EQUAL(mpz_get_si(a->denominator), mpz_get_si(b->denominator));
 	delete(a);
 	delete(b);
 }
